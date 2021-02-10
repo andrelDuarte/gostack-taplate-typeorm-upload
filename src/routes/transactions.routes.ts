1ab7f1ff2b0ae37d220a6395bb21ef5c5a807d48
Andre Luiz D C <and969696@outlook.com>
2021-02-10 11:09:26 -0300

    error: tests with bug

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,33 +1,31 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
 
+import uploadConfig from '../config/uploads';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-
 import DeleteTransactionService from '../services/DeleteTransactionService';
 import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
 
   const transactions = await transactionsRepository.find();
-
   const balance = await transactionsRepository.getBalance();
 
-  return response.json({
-    transactions,
-    balance,
-  });
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
 
-  const createTransaction = new CreateTransactionService();
+  const createTransactionService = new CreateTransactionService();
 
-  const transaction = await createTransaction.execute({
+  const transaction = await createTransactionService.execute({
     title,
     value,
     type,
@@ -47,8 +45,16 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).send();
 });
 
-// transactionsRouter.post('/import', async (request, response) => {
-//   // TODO
-// });
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransaction = new ImportTransactionsService();
+
+    const transactions = await importTransaction.execute(request.file.path);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,20 +1,20 @@
-import { getCustomRepository, TransactionRepository } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 import AppError from '../errors/AppError';
-
-import Transaction from '../models/Transaction';
-
-import Transactionsrepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(id: string): Promise<void> {
-    const transactionsRepository = getCustomRepository(Transactionsrepository);
+  public async execute(transactin_id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
 
-    const transaction = await transactionsRepository.findOne(id);
+    const transactionResponse = await transactionsRepository.findOne(
+      transactin_id,
+    );
 
-    if (!transaction) {
-      throw new AppError('Transaction does not exit');
+    if (!transactionResponse) {
+      throw new AppError('This transaction does not exist!', 404);
     }
-    await transactionsRepository.remove(transaction);
+
+    await transactionsRepository.delete(transactin_id);
   }
 }
 
